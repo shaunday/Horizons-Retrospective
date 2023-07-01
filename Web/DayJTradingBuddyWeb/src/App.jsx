d92dbf9f6cec3f25377153eff051747c4721d264
Shaun <shaun.d@live.com>
2023-07-01 19:26:03 +0300

    web scaffolding

--- Web/DayJTradingBuddyWeb/src/App.jsx
+++ Web/DayJTradingBuddyWeb/src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="vwrapper">
+      <div id="header">Header placeholder</div>
+      <div id="mainBody">
+        <div className="column gotRightSideNeighbour">Left Col</div>
+        <div className="column mainColumn">Right Col</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div id="footer">Footer placeholder</div>
+    </div>
   )
 }
 
