1556d8d51567581220f9d6e42dfde722656125b2
Shaun <shaun.d@live.com>
2024-12-04 14:43:57 +0200

    db tests first steps

--- Server/DayJT.Journal.Test/DatabaseTests.cs
+++ Server/DayJT.Journal.Test/DatabaseTests.cs
@@ -9,20 +9,47 @@ namespace DayJT.Journal.Test
     public sealed class DatabaseTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanInsertTradeComposite()
+        {
+            using (var context = new TradingJournalDataContext(GetDbContextOptions()))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+                //var author = new Author { FirstName = "a", LastName = "b" };
+                //context.Authors.Add(author);
+                ////Debug.WriteLine($"Before save: {author.AuthorId}");
+                //context.SaveChanges();
+                ////Debug.WriteLine($"After save: {author.AuthorId}");
+
+                //Assert.AreNotEqual(0, author.AuthorId);
+            }
+
+        }
+
+
+
+        [TestMethod]
+        public void ChangeTrackerIdentifiesAddedTradeComposite()
+        {
+            using var context = new TradingJournalDataContext(GetDbContextOptions());
+
+            //var author = new Author { FirstName = "a", LastName = "b" };
+            //context.Authors.Add(author);
+            //Assert.AreEqual(EntityState.Added, context.Entry(author).State);
+        }
+
+        private static DbContextOptions<TradingJournalDataContext> GetDbContextOptions()
         {
             var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
+                            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                            .Build();
 
             var builder = new DbContextOptionsBuilder<TradingJournalDataContext>();
             builder.UseNpgsql(configuration.GetConnectionString("DayJTestDatabase"));
-
-            using (var context = new TradingJournalDataContext(builder.Options))
-            {
-                //context.Database.EnsureDeleted();
-                //context.Database.EnsureCreated();
-            }
+            return builder.Options;
         }
     }
 }
+
+     
+    
