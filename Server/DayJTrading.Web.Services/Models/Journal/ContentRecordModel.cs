635375a47515989767973b063820b1dc4c73fdb5
Shaun <shaun.d@live.com>
2024-12-07 01:05:54 +0200

    dtos fix

--- Server/DayJTrading.Web.Services/Models/Journal/ContentRecordModel.cs
+++ Server/DayJTrading.Web.Services/Models/Journal/ContentRecordModel.cs
@@ -2,15 +2,11 @@
 {
     public class ContentRecordModel
     {
-        public int Id { get; set; }
-
         public string Content { get; set; } = string.Empty;
 
         public string ChangeNote { get; set; } = string.Empty;
 
         public DateTime CreatedAt { get; } = DateTime.Now;
 
-
-        public int CellFK { get; set; }
     }
 }
