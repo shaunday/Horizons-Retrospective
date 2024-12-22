216a98ec63a6ba0009cfc95f4f2d8851f5c78673
Shaun <shaun.d@live.com>
2024-12-22 17:47:40 +0200

    startup work

---
+++ Server/JTA.Web.API/Configurations/EnvironmentConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.IdentityModel.Logging;
+
+namespace JTA.Web.API.Configurations
+{
+    public static class EnvironmentConfiguration
+    {
+        public static void ConfigureForEnvironment(this WebApplicationBuilder builder)
+        {
+            if (builder.Environment.IsDevelopment())
+            {
+                IdentityModelEventSource.ShowPII = true;
+            }
+        }
+    }
+
+}
--- Server/JTA.Web.API/Program.cs
+++ Server/JTA.Web.API/Program.cs
@@ -3,7 +3,11 @@ using DayJT.Journal.DataContext.Services;
 using DayJT.Journal.DataSeeder;
 using DayJT.Journal.Repository.Services;
 using DayJTrading.Web.API.Configurations;
+using JTA.Web.API.Configurations;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Logging;
 using Serilog;
+using System;
 
 LoggingConfiguration.ConfigureLogging();
 
@@ -16,6 +20,8 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnC
 #pragma warning disable CS8604 // Disable warning for possible null reference argument
 string? connectionString = builder.Configuration.GetConnectionString("JTA_Db_Key");
 builder.Services.AddInfrastructureWithLogging(connectionString, builder.Environment);
+
+builder.ConfigureForEnvironment();
 
 builder.Services.AddConfiguredControllers();
 builder.Services.AddCustomSwagger();
