59f405fdaa544967b1fc0458c9447787c675546f
Shaun <shaun.d@live.com>
2025-01-22 23:10:29 +0200

    price alerts (server) more scaffolding

--- Server.PriceAlerts/HsR.PriceAlerts.Entities/PriceAlertsGroup.cs
+++ Server.PriceAlerts/HsR.PriceAlerts.Entities/PriceAlertsGroup.cs
@@ -1,7 +1,16 @@
-﻿namespace HsR.PriceAlerts.Entities
+﻿using HsR.Ticker.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace HsR.PriceAlerts.Entities
 {
     public class PriceAlertsGroup
     {
+        [Key]
+        public int Id { get; private set; }
 
+        [Required]
+        public TickerInfo Ticker { get; set; } = null!;
+
+        public List<PriceAlertInfo> PriceAlerts { get; set; } = [];
     }
 }
--- Server.PriceAlerts/HsR.Ticker.Entity/TickerInfo.cs
+++ Server.PriceAlerts/HsR.Ticker.Entity/TickerInfo.cs
@@ -1,7 +1,16 @@
-﻿namespace HsR.Ticker.Entity
-{
-    public class Class1
-    {
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace HsR.Ticker.Entity
+{
+    public class TickerInfo
+    {
+        [Key]
+        public int Id { get; private set; }
+
+        [Required]
+        public string Symbol { get; set; } = null!;
+
+        [Required]
+        public string Name { get; set; } = null!;
     }
 }
