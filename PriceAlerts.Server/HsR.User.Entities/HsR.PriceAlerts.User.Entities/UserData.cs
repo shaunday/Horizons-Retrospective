1d0705e7b85e34fc4574bffd395250b61dfb0b90
Shaun <shaun.d@live.com>
2025-01-31 10:30:26 +0200

    more scaffolding for the price alerts server stuff

---
+++ PriceAlerts.Server/HsR.User.Entities/HsR.PriceAlerts.User.Entities/UserData.cs
@@ -0,0 +1,10 @@
+﻿namespace HsR.PriceAlerts.User.Entities
+{
+    public class UserData
+    {
+        public int Id { get; set; }
+
+        //todo configure
+        public Dictionary<int, DateTime> AlertsLastPushPerTicker { get; set; }
+    }
+}
