2821b1ce4ae0d8442db795cb10b2606f6d6f3326
Shaun <shaun.d@live.com>
2025-03-15 20:48:07 +0200

    trade status tooltip

--- AssetsFlow.Client.React/src/Components/Journal/TradeComposite/Badge/TradeCompositeBadge.jsx
+++ AssetsFlow.Client.React/src/Components/Journal/TradeComposite/Badge/TradeCompositeBadge.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { Badge, ThemeIcon } from "@mantine/core";
+import { Badge, ThemeIcon, Tooltip } from "@mantine/core";
 import { TRADE_STATUS } from "@constants/journalConstants";
 import { getTradeStatusIcon } from "./tradeStatusIcons";
 
 function TradeCompositeBadge({ tradeComposite }) {
     const StatusIcon = getTradeStatusIcon(tradeComposite[TRADE_STATUS]);
+    const statusText = tradeComposite?.[TRADE_STATUS] || "Unknown Status";  // Get status text
 
     return (
-        <Badge size="lg" color="blue.0" p={0}>
-            <ThemeIcon variant="light" >
-                {StatusIcon && <StatusIcon size={18} />}
-            </ThemeIcon>
-        </Badge>
+        <Tooltip label={statusText} withArrow>
+            <Badge size="lg" color="grey.7" p={0}>
+                <ThemeIcon variant="light">
+                    {StatusIcon && <StatusIcon size={18} />}
+                </ThemeIcon>
+            </Badge>
+        </Tooltip>
     );
 }
 
