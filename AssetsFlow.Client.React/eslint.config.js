d547a5104748478c98713e41fd00507bdecdc33e
Shaun <shaun.d@live.com>
2025-08-03 15:14:10 +0300

    workflows, linting

--- AssetsFlow.Client.React/eslint.config.js
+++ AssetsFlow.Client.React/eslint.config.js
@@ -5,11 +5,13 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended', // added import plugin recommended config
+    'prettier', // Ensure this is the last in the extends array
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  settings: { react: { version: 'detect' } },
+  plugins: ['react-refresh', 'import'], // added import plugin here
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
---
+++ AssetsFlow.Service.ChatWiz/eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import nodePlugin from "eslint-plugin-n";
+import importPlugin from "eslint-plugin-import";
+
+export default [
+    js.configs.recommended,
+    {
+        files: ["src/**/*.js", "src/**/*.mjs"],
+        plugins: {
+            n: nodePlugin,
+            import: importPlugin,
+        },
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: "module",
+        },
+        rules: {
+            ...nodePlugin.configs.recommended.rules,
+            ...importPlugin.configs.recommended.rules,
+            // Add your custom rules or overrides here
+        },
+    },
+];
