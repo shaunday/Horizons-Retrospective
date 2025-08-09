2c869af7a7362936746ed25e934b4243dba5f713
Shaun <shaun.d@live.com>
2025-08-09 19:12:48 +0300

    migrations

--- AssetsFlow.Server/AssetsFlow.Journal/HsR.Infrastructure/Migrations/20250809161222_mig1.Designer.cs
+++ AssetsFlow.Server/AssetsFlow.Journal/HsR.Infrastructure/Migrations/20250809161222_mig1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HsR.Journal.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HsR.Journal.Infrastructure.Migrations
 {
     [DbContext(typeof(TradingJournalDataContext))]
-    partial class TradingJournalDataContextModelSnapshot : ModelSnapshot
+    [Migration("20250809161222_mig1")]
+    partial class mig1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AssetsFlow.Server/AssetsFlow.Journal/HsR.Infrastructure/Migrations/20250809161222_mig1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HsR.Journal.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClosedAt",
+                table: "TradeComposites");
+
+            migrationBuilder.DropColumn(
+                name: "OpenedAt",
+                table: "TradeComposites");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "UserData",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiry",
+                table: "UserData",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "UserData");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiry",
+                table: "UserData");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ClosedAt",
+                table: "TradeComposites",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OpenedAt",
+                table: "TradeComposites",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+    }
+}
